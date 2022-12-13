69527e033ccf6516a3a038e10204fdc74a7077a1
Nadya <nb1985@ukr.net>
2022-12-13 19:43:51 +0200

    Header was finishedr

--- src/App.js
+++ src/App.js
@@ -6,33 +6,38 @@ import {BiUserCircle} from 'react-icons/bi';
 function App() {
   return (
     
-<div className='wrapper'>
-    <header>
-      <div className='headerLeft'> 
+<div className='wrapper clear'>
+    <header className='d-flex justify-between p-40'>
+      <div className='d-flex align-center'> 
         <img src = "/img/logo.png" width = {40} height = {40} alt = "logo"/>
           <div className='headerInfo'>
-            <h3>React sneakers</h3>
+            <h3 className='text-uppercase'>React sneakers</h3>
             <p>The shop of the best sneakers</p>
           </div>
-    </div>
+      </div>
 
-      <ul className='headerRight'>
-        <li>
-        <FaShoppingCart className='icon icon-cart'/>
+      <ul className='d-flex justify-between align-center'>
+        <li className='mr-30'>
+        <FaShoppingCart className='icon icon-cart mr-10'/>
           <span>1205 r</span>
         </li>
         <li>
-          <AiOutlineHeart className='icon icon-heart'/>
+          <AiOutlineHeart className='icon icon-heart mr-10'/>
         </li>
         <li>
-          <BiUserCircle className='icon icon-user'/>
+          <BiUserCircle className='icon icon-user mr-10'/>
         </li>
       </ul>
     </header>
 
-    <div className="content">
+    <div className="content p-40">
       <h1>All sneakers</h1>
-      .....
+      <div>
+        
+        <div className="card">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
 
 
