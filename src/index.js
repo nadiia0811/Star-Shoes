119693832c5e2ac23037bd85b63665276c485f7a
Nadya <nb1985@ukr.net>
2022-12-14 20:47:56 +0200

    App.js, index.js were changed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import App from './App';
+import 'macro-css';
+import './index.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
