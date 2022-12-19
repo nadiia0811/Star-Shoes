7beddf9ca5e2d62824dd7b251636bba366c8c616
Nadya <nb1985@ukr.net>
2022-12-19 17:22:41 +0200

    Card component was modified: heart-liked.svg was changed to heart-unliked.svg, object destructuring was used

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,22 @@
 //import '../index.scss';
 
-function Card () {
+function Card (props) {
+  const {name, price, src} = props;
     return(
    
         <div className="card">
           <div className="favorite">
-            <img src="img/heart-liked.svg" alt="Liked" />
+            <img src="img/heart-unliked.svg" alt="Liked" />
           </div>       
-          <img src="/img/sneakers/1.jpg" 
+          <img src={src} 
                alt="Sneakers" 
                width={133}
                height={112}/>
-          <p>Man Sneakers Nike Blazer Mid Suede</p>
+          <p>{name}</p>
           <div className='d-flex justify-between align-center'>
             <div className='d-flex flex-column'>
               <span>Price:</span>
-              <b>129 $</b>
+              <b>{price}</b>
             </div>
            <button className='button'>
               <img src="/img/plus.svg" alt="Plus button" />
