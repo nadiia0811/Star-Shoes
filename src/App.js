5ec1c02c0ec89728d17e7f5cf96ae12a304cc221
Nadya <nb1985@ukr.net>
2022-12-20 20:13:09 +0200

    Was added new state in App: items - to work with items from Mock API. Create Card components from items

--- src/App.js
+++ src/App.js
@@ -5,59 +5,82 @@ import Drawer from './components/Drawer';
 import Header from './components/Header';
 import './index.scss';
 
-const cards = [
+/* const cards = [
   { 
-    name: "Man Sneakers Nike Blazer Mid Suede",
-    price: "129 $",
-    src: "/img/sneakers/1.jpg"
+    "name": "Man Sneakers Nike Blazer Mid Suede",
+    "price": "129 $",
+    "src": "/img/sneakers/1.jpg"
   },
   {
-    name: "Man Sneakers Nike Air Max 270",
-    price: "200 $",
-    src: "/img/sneakers/2.jpg"
+    "name": "Man Sneakers Nike Air Max 270",
+    "price": "200 $",
+    "src": "/img/sneakers/2.jpg"
   },
   {
-    name: "Man Sneakers Nike Blazer Mid Suede",
-    price: "129 $",
-    src: "/img/sneakers/3.jpg"
+    "name": "Man Sneakers Nike Blazer Mid Suede",
+    "price": "129 $",
+    "src": "/img/sneakers/3.jpg"
   },
   {
-    name: "Man Sneakers Puma X Aka Boku Future Rider",
-    price: "119 $",
-    src: "/img/sneakers/4.jpg" 
+    "name": "Man Sneakers Puma X Aka Boku Future Rider",
+    "price": "119 $",
+    "src": "/img/sneakers/4.jpg" 
   },
   {
-    name: "Man Sneakers Мужские Under Armour Curry 8",
-    price: "119 $",
-    src: "/img/sneakers/5.jpg"
+    "name": "Man Sneakers Мужские Under Armour Curry 8",
+    "price": "119 $",
+    "src": "/img/sneakers/5.jpg"
   },
   {
-    name: "Man Sneakers Nike Kyrie 7",
-    price: "129 $",
-    src: "/img/sneakers/6.jpg"
+    "name": "Man Sneakers Nike Kyrie 7",
+    "price": "129 $",
+    "src": "/img/sneakers/6.jpg"
   },
   {
-    name: "Man Sneakers Jordan Air Jordan 11",
-    price: "129 $",
-    src: "/img/sneakers/7.jpg"
+    "name": "Man Sneakers Jordan Air Jordan 11",
+    "price": "129 $",
+    "src": "/img/sneakers/7.jpg"
   },
   {
-    name: "Man Sneakers Nike LeBron XVIII",
-    price: "129 $",
-    src: "/img/sneakers/8.jpg"
+    "name": "Man Sneakers Nike Lebron XVIII Low",
+    "price": "199 $",
+    "src": "/img/sneakers/8.jpg"
+  },
+  {
+    "name": "Man Sneakers Nike Blazer Mid Suede",
+    "price": "129 $",
+    "src": "/img/sneakers/9.jpg"
+  },
+  {
+    "name": "Man Sneakers Nike LeBron XVIII",
+    "price": "129 $",
+    "src": "/img/sneakers/10.png"
+  },
+  {
+    "name": "Man Sneakers Puma X Aka Boku Future Rider",
+    "price": "199 $",
+    "src": "/img/sneakers/11.png"
+  },
+  {
+    "name": "Man Sneakers Nike Kyrie Flytrap IV",
+    "price": "109 $",
+    "src": "/img/sneakers/12.jpg"
   }  
-]
+] */
 
 function App() {
+  const [items, setItems] = useState([]);
   const [cartOpened, setCartOpened] = useState(false);
   
-  return (
-    
-<div className="wrapper clear">
-    {cartOpened ? <Drawer onCloseCart={() => setCartOpened(false)}/> : null}
-    <Header onOpenCart={() => setCartOpened(true)}
-             />
+  fetch('https://63a1cd64ba35b96522e89648.mockapi.io/items')
+      .then(res => res.json())
+      .then(json => setItems(json));      
 
+  return (
+  <div className="wrapper clear">
+    {cartOpened && <Drawer onCloseCart={() => setCartOpened(false)}/> } 
+    <Header onOpenCart={() => setCartOpened(true)} />
+             
     <div className="content p-40">
       <div className='d-flex align-center mb-40 justify-between'>
           <h1>All sneakers</h1>
@@ -68,7 +91,7 @@ function App() {
       </div>
       
       <div className='d-flex justify-between wrap'>
-         {cards.map((item,index) => {
+       {/*   {cards.map((item,index) => {
            const {name, price, src} = item;           
            return  <Card name={name}
                          price={price} 
@@ -78,8 +101,22 @@ function App() {
                          onClickPlus={() => console.log()}
                           />        
          
-           })
-          } 
+           }) 
+          } */}
+
+          
+
+              {items.map((item,index) => {
+                 const {name, price, src} = item;           
+                 return  <Card name={name}
+                               price={price} 
+                               src={src}
+                               key={index}                         
+                               onClickFavorite={() => console.log()}
+                               onClickPlus={() => console.log()} />        
+         
+           }) 
+          }
       </div>
     </div>   
 </div>
